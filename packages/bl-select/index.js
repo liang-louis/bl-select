46ba76f98e1ac082ed612c020edda9f59e865b98
louis <budinger001@qq.com>
2020-11-30 09:27:40 +0800

    feature: 1.0.8

--- packages/bl-select/index.js
+++ packages/bl-select/index.js
@@ -1,10 +1,6 @@
 import BlSelect from './src/bl-select'
-// const BlSelect = {
-//   install: (vue) => {
-//     vue.component('bl-select', BlSelectComp)
-//   }
-// }
+
 BlSelect.install = function(Vue) {
-  Vue.component('bl-select', BlSelect)
+  Vue.component(BlSelect.name, BlSelect)
 }
 export default BlSelect
